74946a7bc1a7e1a426dcd39ee1e535285fe97717
KaashviSharma <87973727+KaashviSharma@users.noreply.github.com>
2022-03-09 19:56:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,55 @@
-var last_position_of_x, last_position_of_y;
-
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
-    var width = screen.width;
-    new_width = screen.width - 70;
-    new_height = screen.height - 300;
-    if(width < 992)
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function(event)
+{
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if(Content == "take my selfie")
     {
-        document.getElementById("myCanvas").width = new_width;
-        document.getElementById("myCanvas").height = new_height;
-        document.body.style.overflow = "hidden";
-    }
-    canvas.addEventListener("touchstart", my_touchstart);
-    
-    function my_touchstart(e)
-    {
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
+    speak();}
+}
 
-        last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
-        last_position_of_y = e.touches[0].clientY - canvas.offsetTop
-    }
+function speak()
+{
+    var synth=window.speechSynthesis;
+    speak_data = "taking your selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        take_snapshot();
+        save();
+    },5000);
+}
 
-    canvas.addEventListener("touchmove", my_touchmove);
-    function my_touchmove(e)
-    {
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format:'png',
+    png_quality:90
+});
 
-         current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
-         current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+camera= document.getElementById("camera");
 
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
 
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
-        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
-        ctx.stroke();
-        last_position_of_x = current_position_of_touch_x;
-        last_position_of_y = current_position_of_touch_y;
-    }
-function clearArea(){
-    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
+function save()
+{
+    link= document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click();
 }
